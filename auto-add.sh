368c1b70b152f73d2a09c06ded8c44e4f1e6f3cd
KangRio <kang.rio123@gmail.com>
2018-01-06 11:57:15 +0700

    Add files via upload

---
+++ auto-add.sh
@@ -0,0 +1,21 @@
+
+read -p "usermane : " Login
+read -p "Password : " Passwd
+read -p "Proxy : " Proxy
+read -p "IP : " IP
+
+echo "<auth-user-pass>" >> /sdcard/ovpn/file/*
+echo "$Login" >> /sdcard/ovpn/file/*
+echo "$Passwd" >> /sdcard/ovpn/file/*
+echo "</auth-user-pass>" >> /sdcard/ovpn/file/*
+echo "remote $IP:1194@lvs.truehits.in.th" >> /sdcard/ovpn/file/*
+echo "http-proxy $Proxy 8080" >> /sdcard/ovpn/file/*
+echo "http-proxy-retry 
+http-proxy-timeout 3 
+redirect-gateway local def1 
+redirect-gateway local def1 bypass-dhcp 
+dhcp-option DNS 8.8.8.8 
+dhcp-option DNS 8.8.4.4 
+dhcp-option DOMAIN google.com" >> /sdcard/ovpn/file/*
+
+sed '4d' /sdcard/ovpn/file/* > /sdcard/ovpn/new.ovpn
